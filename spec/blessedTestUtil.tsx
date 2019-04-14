74a5b1004aedd8daee98609f9e21468c29808be8
Sebastián Gurín <sgurin@awalabs.com>
2019-04-14 16:44:53 -0300

    green

--- spec/blessedTestUtil.tsx
+++ spec/blessedTestUtil.tsx
@@ -2,7 +2,6 @@ import * as blessed from 'blessed'
 import { installExitKeys } from '../src/blessed/blessed'
 import { Element, Screen } from '../src/blessed/blessedTypes'
 import { React } from '../src/blessed/jsx/createElement'
-import { screen } from 'blessed';
 
 export function testJsx({
   creator,
@@ -29,34 +28,45 @@ export function testElement({
   creator: (screen: Screen) => Element
   assert: (e: Element) => void
 }) {
-  const screen = blessed.screen({ smartCSR: true, debug: true, log: './log.txt'})
-  installExitKeys(screen)
-  const a = creator(screen)
-  a.on('render', () => {
-    assert(a)
-    screen!.destroy()
-  })
-  screen.render()
+  let screen: Screen
+  try {
+    screen = blessed.screen({ smartCSR: true, debug: true, log: 'log.txt' })
+    installExitKeys(screen)
+    const a = creator(screen)
+
+    setTimeout(() => {
+      // HEADS UP _ we need to timeout instead of lietening to render becase this i triggered twice the first time the workrges are not yet rendered....
+  assert(a)
+  screen!.destroy()
+  }, 100);
+
+    // a.on('render', () => {
+    // })
+    screen.render()
+  } catch (error) {
+    screen!.log(error)
+  }
+
 }
 
 /** install beforeEach and afterEach so they create a new screen for its spec and destroy previous */
-export function installBeforeAndAfterMethods(options: Options){
+export function installBeforeAndAfterMethods(options: Options) {
   let screen: Screen
-  beforeEach(()=>{
-    if(screen && !screen.destroyed){
+  beforeEach(() => {
+    if (screen && !screen.destroyed) {
       screen.destroy()
     }
     screen = blessed.screen({ smartCSR: true })
   })
-  afterEach(()=>{
-    if(screen && !screen.destroyed){
+  afterEach(() => {
+    if (screen && !screen.destroyed) {
       screen.destroy()
     }
   })
 }
 
 /** a driver manages a screen and a test session */
-export class TestDriver{
+export class TestDriver {
   private _screen: Screen | undefined;
   public get screen(): Screen {
     return this.getScreen()
@@ -66,35 +76,36 @@ export class TestDriver{
     // }
     // return this._screen!;
   }
-  constructor(protected options:Options = {}){
+  constructor(protected options: Options = {}) {
   }
 
-  private getScreen(mode: 'nothing'|'reset'|'createNew'|'destroy' = 'nothing'){
-    if(this._screen&&(!this._screen.destroyed&&mode==='createNew'||mode==='destroy')){
+  private getScreen(mode: 'nothing' | 'reset' | 'createNew' | 'destroy' = 'nothing') {
+    if (this._screen && (!this._screen.destroyed && mode === 'createNew' || mode === 'destroy')) {
       this._screen.destroy()
       this._screen = undefined
     }
-    if(!this._screen||mode==='createNew'||this._screen.destroyed){
-      this. _screen = blessed.screen({ smartCSR: true, debug: true, log: './log.txt'})
+    if (!this._screen || mode === 'createNew' || this._screen.destroyed) {
+      this._screen = blessed.screen({ smartCSR: true, debug: true, log: './log.txt' })
       installExitKeys(this._screen)
     }
-    else if(mode==='reset') {
+    else if (mode === 'reset') {
       this._screen!.leave() //TODO HEADS UP : test if this work!
       // TODO events and descendants ? 
     }
     return this._screen
   }
 
-  installBeforeAndAfterMethods(options: Options={}){
-    this.options = {...this.options, ...options}
+  installBeforeAndAfterMethods(options: Options = {}) {
+    this.options = { ...this.options, ...options }
     this._screen = this.getScreen()
     const before = options.installBeforeAfterAll ? beforeAll : beforeEach
-    before(()=>{
-      this.getScreen(options.reuseScreen ? 'reset' :'createNew'
-      )})
-      const after = options.installBeforeAfterAll ? afterAll : afterEach
-      after(()=>{
-        this.getScreen('destroy')
+    before(() => {
+      this.getScreen(options.reuseScreen ? 'reset' : 'createNew'
+      )
+    })
+    const after = options.installBeforeAfterAll ? afterAll : afterEach
+    after(() => {
+      this.getScreen('destroy')
     })
     // return this.getScreen()
   }
--- spec/collapsibleSpec.ts
+++ spec/collapsibleSpec.ts
@@ -1,13 +1,10 @@
-import * as blessed from 'blessed'
-import * as contrib from 'blessed-contrib'
-import { installExitKeys, onValueChange } from '../src/blessed/blessed'
-import { BoxOptions, Screen, Markdown } from '../src/blessed/blessedTypes'
-import { installCollapsible, toggleCollapsed, uninstallCollapsible } from '../src/blessed/collapsible'
-import { renderer } from '../src/blessed/layoutRenderer'
-import { testElement } from './blessedTestUtil';
+import * as blessed from 'blessed';
+import * as contrib from 'blessed-contrib';
+import { BoxOptions, Markdown, Screen } from '../src/blessed/blessedTypes';
+import { installCollapsible } from '../src/blessed/collapsible';
 import { find } from '../src/blessed/node';
-import { screen } from 'blessed';
 import { strip } from '../src/util/misc';
+import { testElement } from './blessedTestUtil';
 
 describe('collapsible', () => {
   it('should allow zero configuration with auto property', async done => {
@@ -34,8 +31,8 @@ describe('collapsible', () => {
           height: '100%',
           border: 'line',
         })
-      installCollapsible(layout, { auto: true })
-         contrib.markdown({
+        installCollapsible(layout, { auto: true })
+        contrib.markdown({
           ...checkboxOptions,
           parent: layout,
           markdown
@@ -43,31 +40,13 @@ describe('collapsible', () => {
         return layout
       },
       assert(e) {
-        // e.screen.log('hs khsdfjkh')
-        // e.screen.log('hs khsdfjkh')
-        // e.screen.log('hs khsdfjkh')
-        // e.screen.log('hs khsdfjkh')
-        // console.log(find<Markdown>(e, c => c.type === 'markdown')!);   
-        // e.screen.log('hshshshs\n\n\nsdkjhjhksdfjhksfdjhksdfjkhsdfjkh')
         const el = find<Markdown>(e, c => c.type === 'markdown')!
-        // e.screen.log('heleehkekhke', el && el.getContent())
-        markdown.split('\n').map(s=>strip(s).trim()).forEach(l=>{
+        markdown.split('\n').map(s => strip(s).trim()).forEach(l => {
           expect(el.getContent()).toContain(l)
         })
-        // console.log(find<Markdown>(e, c => c.type === 'markdown')!.getContent())
-        // expect(find<Markdown>(e, c => c.type === 'markdown')!.getContent()).toContain('when the collapsible checkbox above')
-        // console.log(find<Textbox>(e, c => isElement<Textbox>(c) && c.secret)!.getContent());
-        // expect(find<Textbox>(e, c => isElement<Textbox>(c) && c.secret)!.getContent()).toContain('helelele')
-        // done()
+        done()
       }
     })
   })
 })
 
-
-
-// const screen = blessed.screen({ smartCSR: true, log: './log.txt' })
-
-// function creator
-
-// creator(screen)
